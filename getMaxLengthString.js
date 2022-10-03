c616e7e222bcc8be8ea0cbec48e4de2bcd326bdb
NewBww <bowonwit.a@gmail.com>
2022-10-03 21:48:18 +0700

    fix

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -6,9 +6,15 @@ function getMaxLengthString(arrayOfString) {
   arrayOfString.forEach(s => {
     if (s.length > maxStringLength) maxStringLength = s.length;
   });
+  // for (const resultArrayElement of arrayOfString) {
+  //   if (resultArrayElement.length > maxStringLength)
+  //     maxStringLength = resultArrayElement.length
+  //   }
+  // }
   for (i = 0, count = 0; i < arrayOfString.length; i++) {
     if (arrayOfString[i].length === maxStringLength) resultArray[count++] = arrayOfString[i]
   }
   return resultArray;
 }
 module.exports = getMaxLengthString
+
